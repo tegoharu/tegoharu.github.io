2475dd8afaddabb16945890f6b045a0f41396d28
曽根 陽香 <h-sone@h-sone-Mac2.local>
2018-05-18 12:55:13 +0900

    test

--- send.php
+++ send.php
@@ -1,9 +1,21 @@
 <!doctype html>
 <?php
-$message = "名前：".$_POST["name"]."\n本文：".$_POST["message"];
-
-if(!mb_send_mail("onepiece@sakura-ic.com",$message)){
-	exit("error");
+$action = $_POST['action'];
+$name = htmlspecialchars($_POST['name']);
+$message = htmlspecialchars($_POST['message']);
+$to = 'onepiece@sakura-ic.com';
+$subject = 'お問い合わせ';
+$comment = '[お名前]'."\n".$name."\n";
+$comment = '[本文]'."\n".$message."\n\n\n";
+if($action == "post"){
+	$status = mb_send_mail($to, $subject, $comment);
+	if($status) {
+		echo '<p>メッセージは正常に送信されました</p>';
+		echo '<button type="button" onclick="history.go(-1)">フォームに戻る</button>';
+	} else {
+		echo '<p>メッセージの送信に失敗しました</p>';
+		echo '<button type="button" onclick="history.go(-1)">フォームに戻る</button>';
+	}
 }
 ?>
 <html>
